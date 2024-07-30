f8a511ecdf7bcf7ec9705e09c924018dcb034ed1
longvu19 <longvd.dev@gmail.com>
2024-07-31 05:38:14 +0700

    WIP

--- src/components/base/SearchResultPopup.vue
+++ src/components/base/SearchResultPopup.vue
@@ -1,116 +1,140 @@
 <script lang="ts" setup>
-import { computed, onMounted } from "vue";
-import { ComputedRef, onUnmounted } from "vue";
-import { getMovieSearchResult } from "../../services/movieService";
-import type {
-  MovieSearchResultResponse,
-  MovieListInfo,
-} from "../../services/types";
-import SearchResult from "./SearchResult.vue";
-const props = defineProps<{
-  keyword: string;
-  isMobile?: boolean;
-}>();
-const abortController = new AbortController();
-onUnmounted(() => {
-  abortController.abort();
-})
-const searchResult: MovieSearchResultResponse | null = await getMovieSearchResult(props.keyword, 10, abortController);
-const data: ComputedRef<MovieListInfo[]> = computed(() => {
-  return searchResult ? searchResult.data.items : [];
-});
-
-const emits = defineEmits<{
-  (e: 'closePopup', event: Event): void
-}>();
-
-onMounted(() => {
-  window.addEventListener("click", (e) => {
-    emits("closePopup", e);
-  }, {
-    once: true
+  import Loading from "./Loading.vue";
+  import { computed, onMounted, onUnmounted, onUpdated } from "vue";
+  import type { ComputedRef } from "vue";
+  import { getMovieSearchResult } from "../../services/movieService";
+  import type {
+    MovieSearchResultResponse,
+    MovieListInfo,
+  } from "../../services/types";
+  import SearchResult from "./SearchResult.vue";
+  const props = defineProps<{
+    keyword: string;
+    isMobile?: boolean;
+    stopRequest?: boolean
+  }>();
+  const abortController = new AbortController();
+  onUnmounted(() => {
+    abortController.abort();
   })
-})
+  const footerLinkClassObject = computed(() => {
+    return { "search-result__footer-link": props.isMobile }
+  })
+  const stopRequestPromise : Promise<null> = new Promise((resolve) => {
+    if(props.stopRequest) {
+      resolve(null);
+    }
+  })
+  const searchResult: MovieSearchResultResponse | null = props.stopRequest ? await stopRequestPromise : await getMovieSearchResult(props.keyword, 10, abortController);
+  const data: ComputedRef<MovieListInfo[]> = computed(() => {
+    return searchResult ? searchResult.data.items : [];
+  });
+
+  const emits = defineEmits<{
+    (e: 'closePopup', event: Event): void
+  }>();
+
+  const loadingClassObject = computed(() => {
+    return { 'search-box__loading--mobile': props.isMobile }
+  })
+
+  onMounted(() => {
+    window.addEventListener("click", (e) => {
+      emits("closePopup", e);
+    }, {
+      once: true
+    })
+  })
+  onUpdated(() => {
+    if (props.stopRequest) {
+      abortController.abort();
+    }
+  })
 </script>
 
 <template>
-  <div v-if="searchResult" class="search-result" :class="{ 'search-result--mobile': props.isMobile }">
-    <div class="search-result__list-container">
-      <SearchResult :result="data" :imgHost="searchResult.data.APP_DOMAIN_CDN_IMAGE" v-if="data.length > 0" />
-      <div v-else class="search-result__no-result">Không tìm thấy kết quả cho "{{ keyword }}"</div>
+  <Suspense>
+    <div v-if="searchResult" class="search-result" :class="{ 'search-result--mobile': props.isMobile }">
+      <div class="search-result__list-container">
+        <SearchResult :result="data" :imgHost="searchResult.data.APP_DOMAIN_CDN_IMAGE" v-if="data.length > 0" />
+        <div v-else class="search-result__no-result">Không tìm thấy kết quả cho "{{ keyword }}"</div>
+      </div>
+      <div class="search-result__footer" v-if="data.length > 0">
+        <RouterLink :to="{ name: 'tim-kiem', query: { k: keyword } }" class="search-result__footer-link" :class="footerLinkClassObject">
+          Xem thêm
+        </RouterLink>
+      </div>
     </div>
-    <div class="search-result__footer" v-if="data.length > 0">
-      <RouterLink :to="{ name: 'tim-kiem', query: { k: keyword } }" class="search-result__footer-link" :class="{ 'search-result__footer-link--mobile': props.isMobile }">
-        Xem thêm
-      </RouterLink>
-    </div>
-  </div>
+    <template #fallback>
+      <Loading class="search-box__loading" :class="loadingClassObject" size="30px" type="square" />
+    </template>
+  </Suspense>
 </template>
 
 <style lang="scss" scoped>
-$className: "search-result";
+  $className: "search-result";
 
-.#{$className} {
-  display: flex;
-  flex-direction: column;
-  position: absolute;
-  top: 100%;
-  left: 0;
-  width: 100%;
-  background: rgb(17, 17, 17);
-
-  &__no-result {
-    text-align: center;
-    color: white;
-    padding: 10px;
-    height: 200px;
+  .#{$className} {
     display: flex;
-    align-items: center;
-    justify-content: center;
-  }
-
-  &--mobile {
-    display: flex;
-    position: static;
-    flex-grow: 1;
-    background: transparent;
-    height: calc(100% - 70px);
-
-    .#{$className}__list-container {
-      max-height: unset;
-      flex-grow: 1;
-    }
-
-    .#{$className}__no-result {
-      height: 100%;
-    }
-  }
-
-  &__list-container {
-    max-height: 380px;
-    overflow: auto;
-  }
-
-  &__footer {
-    display: flex;
-  }
-
-  &__footer-link {
-    display: block;
+    flex-direction: column;
+    position: absolute;
+    top: 100%;
+    left: 0;
     width: 100%;
-    padding: 10px 0;
-    text-decoration: none;
-    color: white;
-    text-align: center;
     background: rgb(17, 17, 17);
 
-    &--mobile {
-      padding: 20px 0;
+    &__no-result {
+      text-align: center;
+      color: white;
+      padding: 10px;
+      height: 200px;
+      display: flex;
+      align-items: center;
+      justify-content: center;
     }
 
-    &:hover {
-      background: rgb(64, 64, 64);
+    &--mobile {
+      display: flex;
+      position: static;
+      flex-grow: 1;
+      background: transparent;
+      height: calc(100% - 70px);
+
+      .#{$className}__list-container {
+        max-height: unset;
+        flex-grow: 1;
+      }
+
+      .#{$className}__no-result {
+        height: 100%;
+      }
+    }
+
+    &__list-container {
+      max-height: 380px;
+      overflow: auto;
+    }
+
+    &__footer {
+      display: flex;
+    }
+
+    &__footer-link {
+      display: block;
+      width: 100%;
+      padding: 10px 0;
+      text-decoration: none;
+      color: white;
+      text-align: center;
+      background: rgb(17, 17, 17);
+
+      &--mobile {
+        padding: 20px 0;
+      }
+
+      &:hover {
+        background: rgb(64, 64, 64);
+      }
     }
   }
-}
 </style>
--- src/utils/helper.ts
+++ src/utils/helper.ts
@@ -2,13 +2,17 @@ export function arrayToString(array: any[], separator: string = ", "): string {
   return array.map((obj) => (obj.name ? obj.name : obj)).join(separator);
 }
 
-export function debounce(func: Function, duration: number): any {
+export function debounce(func: Function, duration: number, leading: boolean = false): any {
   let timeout: ReturnType<typeof setTimeout> | null;
   return (...args: any) => {
     const effect = () => {
       timeout = null;
       return func(args);
     };
+
+    if (!timeout && leading) {
+      effect()
+    }
 
     if (timeout) {
       clearTimeout(timeout);
